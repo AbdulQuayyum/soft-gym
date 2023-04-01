c412c21bcb8f996902a875db4f413f81015b1b12
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-04-02 00:36:05 +0100

    Cooking in progress

--- src/Routes/Main.Routes.jsx
+++ src/Routes/Main.Routes.jsx
@@ -1,20 +1,14 @@
 import React from 'react'
-import { Box } from '@mui/material';
 import { Routes, Route } from "react-router-dom"
 
 import { ExerciseDetail, Home } from "../Views/Index"
-import { Footer, Navbar } from "../Components/Index"
 
 const MainRoutes = () => {
     return (
-        <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/Exercise/:id" element={<ExerciseDetail />} />
-            </Routes>
-            <Footer />
-        </Box>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Exercise/:id" element={<ExerciseDetail />} />
+        </Routes>
     )
 }
 
