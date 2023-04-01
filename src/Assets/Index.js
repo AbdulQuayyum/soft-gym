a991d7919e5584b36ce6fb5a18373ac197213624
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-04-02 00:01:46 +0100

    Made changes...

---
+++ src/Assets/Index.js
@@ -0,0 +1,13 @@
+import BodyPart from "./Icons/body-part.png"
+import Equipments from "./Icons/equipments.png"
+import Gym from "./Icons/gym.png"
+import LeftArrow from "./Icons/left-arrow.png"
+import RightArrow from "./Icons/right-arrow.png"
+import Target from "./Icons/target.png"
+
+import Banner from "./Images/banner.jpg"
+import Logo from "./Images/logo.png"
+
+export {
+    BodyPart, Equipments, Gym, LeftArrow, RightArrow, Target, Banner, Logo
+}
