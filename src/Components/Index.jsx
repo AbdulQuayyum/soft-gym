19f496f96aec8867bc3b6291e355d8e6aed57aea
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-04-02 01:01:50 +0100

    Cooker is cooking

--- src/Components/Index.jsx
+++ src/Components/Index.jsx
@@ -1,4 +1,25 @@
+import BodyPart from "./BodyPart";
+import Detail from "./Detail";
+import ExerciseCard from "./ExerciseCard";
+import ExerciseVideos from "./ExerciseVideos";
 import Footer from "./Footer";
+import HeroBanner from "./HeroBanner";
+import HorizontalScrollbar from "./HorizontalScrollbar";
+import Loader from "./Loader";
 import Navbar from "./Navbar";
+import SearchExercises from "./SearchExercises";
+import SimilarExercies from "./SimilarExercies";
 
-export { Footer, Navbar }
+export {
+    BodyPart,
+    Detail,
+    ExerciseCard,
+    ExerciseVideos,
+    Footer,
+    HeroBanner,
+    HorizontalScrollbar,
+    Loader,
+    Navbar,
+    SearchExercises,
+    SimilarExercies
+}
